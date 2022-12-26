c3b5b8b62972f4722d50cb4886f68013b6b14904
Bibhor2000 <daxter740@gmail.com>
2022-12-26 17:07:13 -0500

    fixed rendering model issue by using the absolute path to the file rather than its relative path in line 7 on model js

--- src/components/Model.js
+++ src/components/Model.js
@@ -4,14 +4,13 @@ import '../App.css'
 const Model = () => (
     <div className='card'>
         <model-viewer
-          src='public/egx.glb'
+          src='/egx.glb'
           ios-src=''
           poster=''
           alt=''
           shadow-intensity=''
           camera-controls
           auto-rotate
-          ar
         ></model-viewer>
     </div>
 )
